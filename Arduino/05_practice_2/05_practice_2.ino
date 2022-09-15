30e430cbcc2b330404a36f7e173f42ab6f752e88
1hanys <105336867+1hanys@users.noreply.github.com>
2022-09-15 14:39:36 +0900

    05_practice_2

---
+++ Arduino/05_practice_2/05_practice_2.ino
@@ -0,0 +1,29 @@
+#define PIN_LED 7
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT); 
+
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(800);
+  digitalWrite(PIN_LED, 1);
+  delay(200);
+
+  int i = 0;
+  while (i < 5){
+    digitalWrite(PIN_LED, 0);
+    delay(200);
+    digitalWrite(PIN_LED, 1);
+    delay(200);
+
+    i ++;
+    }
+    
+  
+  while(1){
+  digitalWrite(PIN_LED, 1);
+  }
+
+}
